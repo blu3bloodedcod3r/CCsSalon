900429818c7569ea9a959a6844c678bbced51625
Niloufar Sabbaghian <niloufar@Niloufars-MacBook-Pro.local>
2023-01-26 20:02:53 -0800

    password hash-validate

--- Salon/server/models/User.js
+++ Salon/server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const { Schema } = mongoose;
 
@@ -29,6 +30,21 @@ const UserSchema = new Schema({
     },
   },
 );
+
+// hash user password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model('User', UserSchema);
 

073b91be78c4e2686dbf15b2446d45ad104de863
spmfd <110492974+spmfd@users.noreply.github.com>
2023-01-26 20:11:49 -0800

    worked on booking page and array being accessible

--- Salon/client/src/pages/home.js
+++ Salon/client/src/pages/home.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { products } from './products';
 
-export default function Product(product) {
+export default function Product() {
+
+    const product = products[0]
 
     return (
         <div className="card">
-            <img src={product.image} className="card-img-top" alt="Service Picture Placeholder"></img>
+            <img src={products.image} className="card-img-top" alt="Service Picture Placeholder"></img>
                 <div className="card-body">
-                    <h5 className="card-title"><strong>{product.name}</strong></h5>
+                    <h5 className="card-title"><strong>{products.name}</strong></h5>
                     <p className="card-text">
                         <ul>
-                            <li>Description: {product.description}</li>
-                            <li>Duration: {product.duration}</li>
-                            <li>Price: {product.price}</li>
+                            <li>Description: {products.description}</li>
+                            <li>Duration: {products.duration}</li>
+                            <li>Price: {products.price}</li>
                         </ul>
                     </p>
                 <a href="/book" className="btn btn-primary">Reserve an appointment now</a>

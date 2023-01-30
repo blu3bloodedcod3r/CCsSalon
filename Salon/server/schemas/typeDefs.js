31cd4bb24b666e274aee0eb1162f15e921a8e38a
Niloufar Sabbaghian <niloufar@Niloufars-MacBook-Pro.local>
2023-01-29 21:51:58 -0800

    mutations added

--- Salon/server/schemas/typeDefs.js
+++ Salon/server/schemas/typeDefs.js
@@ -9,19 +9,19 @@ type User {
   appts: [Appt]
 }
 type Services {
-    _id: ID
-    name: String!
-    description: String
-    price: String
-    duration: String
-    filename: String
+  _id: ID
+  name: String!
+  description: String
+  price: String
+  duration: String
+  filename: String
 }
 type Appt {
-    _id: ID
-    date: String
-    time: String
-    message: String
-    service: Services
+  _id: ID
+  date: String
+  time: String
+  message: String
+  service: Services
 }
 type Auth {
   token: ID
@@ -31,6 +31,15 @@ type Auth {
 type Query {
   services: [Services]
   user: User
+  appts: [Appt]
+}
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(name: String!, email: String!, password: String!): Auth
+  makeAppt(date: String, time: String, Message: String, service: ID!): User
+  deleteAppt(_id: ID!): Appt
+  addServices(name: String!, description: String, price: String, duration: String, filename: String): Services
+  deleteServices(_id: ID!): Services
 }
 `;
 

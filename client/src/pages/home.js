27f7b2a5dbc45edb4683c74975ddd1ca45a58d8f
spmfd <110492974+spmfd@users.noreply.github.com>
2023-02-07 09:16:35 -0800

    images

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -19,7 +19,7 @@ const Service = () => {
         {services.map((service) => (
           <div className="card" key={service.id}>
             <img
-              src={service.image}
+              src={require(`./images/${service.image}`)}
               className="card-img-top"
               alt="Service Placeholder"
             ></img>
